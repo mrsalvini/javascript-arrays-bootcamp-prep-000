6d197fda29a2b9090e1ccef04b4a6f69fee30d8f
Diogo Costa <dsgmcosta@gmail.com>
2019-02-02 17:00:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,4 +30,26 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(chocolateBars)
